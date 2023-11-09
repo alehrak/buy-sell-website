ee413c738d5007212db26600eb27980431feb156
alehrak <aleh.rak@gmail.com>
2023-11-09 15:08:46 +0100

    initialization of the project

---
+++ src/main/java/com/gmail/rak/aleh/BuySellWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.rak.aleh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySellWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuySellWebsiteApplication.class, args);
+	}
+}
